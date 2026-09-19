dfd90bf188f8544fa632bad09c8a046b78f9b037
WenWu6 <Wwenbing2024@outlook.com>
2026-09-19 10:21:02 +0800

    Implement SpiritPowerMod main class

---
+++ src/main/java/com/example/spiritpower/SpiritPowerMod.java
@@ -0,0 +1,26 @@
+package com.example.spiritpower;
+
+import com.example.spiritpower.network.NetworkHandler;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(SpiritPowerMod.MODID)
+public class SpiritPowerMod {
+    public static final String MODID = "spiritpower";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public SpiritPowerMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(NetworkHandler::register);
+    }
+}
